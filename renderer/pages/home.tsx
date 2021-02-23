6c1b56f34db0ab843792bfaa97af6998ade244e3
apsampaio <andre03.sampaio@gmail.com>
2021-02-23 19:18:28 -0300

    add countdown

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import Head from "next/head";
+
 import ExperienceBar from "../components/ExperienceBar";
 import Profile from "../components/Profile";
+import CompletedChallenges from "../components/CompletedChallenges";
+import Countdown from "../components/Countdown";
 
 import styles from "../styles/pages/Home.module.css";
 
@@ -15,6 +18,8 @@ const Home = () => {
       <section>
         <div>
           <Profile />
+          <CompletedChallenges />
+          <Countdown />
         </div>
         <div></div>
       </section>
